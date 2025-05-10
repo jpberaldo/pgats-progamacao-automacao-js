7033da7fd1a270358861956dcba20718fe1c9be8
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2025-05-10 10:01:21 -0300

    feat/adicionado exercicio-distribuir-brinquedo

---
+++ exercicio-distribuir-brinquedo.js
@@ -0,0 +1,13 @@
+
+let listaBrinquedos = [{ brinquedo: "bola", quantidade: 5 }, { brinquedo: "osso", quantidade: 2 }, { brinquedo: "corda", quantidade: 20 },
+    {brinquedo: "sino", quantidade: 8}
+]
+
+distribuirBrinquedo(listaBrinquedos);
+
+function distribuirBrinquedo(lista) {
+
+    lista.forEach(lista => {
+        console.log(`distribuindo ${lista.quantidade} ${lista.brinquedo}(s) `);
+    });
+}
