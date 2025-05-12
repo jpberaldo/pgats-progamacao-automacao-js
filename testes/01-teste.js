d6264143765b07f5c1929b3c09bb66024d6bbd09
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2025-05-12 03:06:53 -0300

    feat/adicionando arquivo de exemplo teste

---
+++ testes/01-teste.js
@@ -0,0 +1,11 @@
+
+// describe = agrupador de testes, recebe o nome e a função
+// it = implementacao do teste
+
+describe('Testes do projeto', () => {
+
+    it('primeiro teste', () => {
+
+    })
+
+})
